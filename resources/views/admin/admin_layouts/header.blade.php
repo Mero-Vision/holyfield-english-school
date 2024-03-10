9b32c8c759f96652ebcf6a5697ca3daac77581ef
Hancie123 <hanciewanemphago@gmail.com>
2024-03-10 18:13:55 +0545

    login added

--- resources/views/admin/admin_layouts/header.blade.php
+++ resources/views/admin/admin_layouts/header.blade.php
@@ -3,18 +3,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
     <title>Holyfield English School</title>
 
-    <link rel="shortcut icon" href="assets/img/logo.jpg">
-
-   
-
-    <link rel="stylesheet" href="{{url('assets/admin/css/bootstrap.min.css')}}">
-
-    <link rel="stylesheet" href="{{url('assets/admin/css/feather.css')}}">
-
-    <link rel="stylesheet" href="{{url('assets/admin/css/flags.css')}}">
-
-    <link rel="stylesheet" href="{{url('assets/admin/css/fontawesome.min.css')}}">
-    <link rel="stylesheet" href="{{url('assets/admin/css/all.min.css')}}">
-
-    <link rel="stylesheet" href="{{url('assets/admin/css/style.css')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('assets/school/img/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('assets/school/img/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('assets/school/img/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ url('assets/school/img/site.webmanifest') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/feather.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/flags.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/admin/css/style.css') }}">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 </head>
