082d70efee406b6c1531b14f5fb92f9a5e5f0f4d
Hancie123 <hanciewanemphago@gmail.com>
2024-03-14 23:00:19 +0545

    blog added

--- app/Http/Controllers/School/SchoolBlogController.php
+++ app/Http/Controllers/School/SchoolBlogController.php
@@ -4,11 +4,12 @@ namespace App\Http\Controllers\School;
 
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
+use App\Models\SiteSetting;
 use Illuminate\Http\Request;
 
 class SchoolBlogController extends Controller
 {
-    public function index(){
+    public function index($settingable_type = null, $settingable_id = null){
 
         $search=request()->query('search_keyword');
         
@@ -21,13 +22,39 @@ class SchoolBlogController extends Controller
 
         
         $recentBlogs = Blog::with('media')->where('status', 'active')->latest()->paginate(5);
-        return view('school.blogs',compact('blogs', 'recentBlogs'));
+
+        $site_setting = SiteSetting::where("settingable_type", $settingable_type)
+            ->where("settingable_id", $settingable_id)
+            ->get();
+        $data = [];
+        foreach ($site_setting as $item) {
+            if ($item->type == 'image') {
+                $data[$item->key] = $item->getFirstMediaUrl();
+            } else {
+                $data[$item->key] = $item->value;
+            }
+        }
+        
+        return view('school.blogs',compact('blogs', 'recentBlogs','data'));
     }
 
-    public function show($slug)
+    public function show($slug, $settingable_type = null, $settingable_id = null)
     {
         $blog = Blog::with('media')->where('slug', $slug)->first();
         $recentBlogs = Blog::with('media')->where('status', 'active')->latest()->paginate(5);
-       return view('school.blog_details',compact('blog', 'recentBlogs'));
+
+        $site_setting = SiteSetting::where("settingable_type", $settingable_type)
+            ->where("settingable_id", $settingable_id)
+            ->get();
+        $data = [];
+        foreach ($site_setting as $item) {
+            if ($item->type == 'image') {
+                $data[$item->key] = $item->getFirstMediaUrl();
+            } else {
+                $data[$item->key] = $item->value;
+            }
+        }
+        
+       return view('school.blog_details',compact('blog', 'recentBlogs','data'));
     }
 }
